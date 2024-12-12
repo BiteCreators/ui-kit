c6f5dddba3b0c4b90ce884a282cc61c0bba8d39e
tapuru <pogladtapira@gmail.com>
2024-12-12 16:49:13 +0500

    fix: fix null hooks issue

--- esbuild.config.cjs
+++ esbuild.config.cjs
@@ -8,9 +8,10 @@ require("esbuild")
   .build({
     entryPoints: ["src/index.ts", "./src/assets/icons/components/index.ts"], // entry point of your code
     bundle: true, // bundle all dependencies
-    platform: "neutral", // specify platform (node for backend, browser for frontend)
-    target: "esnext", // for modern JavaScript (can be adjusted if needed)
+    platform: "browser", // specify platform (node for backend, browser for frontend)
+    target: "es2015", // for modern JavaScript (can be adjusted if needed)
     outdir: "dist",
+    jsx: "automatic",
     format: "esm",
     minify: true, // optional, you can set this to false if you don’t want minification
     sourcemap: false, // optional, to generate sourcemaps
@@ -24,6 +25,11 @@ require("esbuild")
       "zlib",
       "stream",
       "react-google-recaptcha",
+      "react",
+      "react-dom",
+      "next",
+      "next/link",
+      "react/jsx-runtime",
     ], // specify external dependencies if needed
     loader: {
       ".ts": "ts", // Load TypeScript files
