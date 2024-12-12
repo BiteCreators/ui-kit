ebcb7eaa22dcbd3f2e3c074c9819a8ff31e889aa
fireonex <mari.krulikovskaya@mail.ru>
2024-12-12 15:34:41 +0300

    fix: double compilation in esm and cjs

--- esbuild.config.cjs
+++ esbuild.config.cjs
@@ -1,41 +1,56 @@
 const { execSync } = require("child_process");
 
 function generateDeclarations() {
-  execSync("npx tsc --emitDeclarationOnly", { stdio: "inherit" });
+    execSync("npx tsc --emitDeclarationOnly", { stdio: "inherit" });
 }
 
-require("esbuild")
-  .build({
-    entryPoints: ["src/index.ts", "./src/assets/icons/components/index.ts"], // entry point of your code
-    bundle: true, // bundle all dependencies
-    platform: "browser", // specify platform (node for backend, browser for frontend)
-    target: "es2015", // for modern JavaScript (can be adjusted if needed)
-    outdir: "dist",
-    jsx: "automatic",
-    format: "esm",
-    minify: true, // optional, you can set this to false if you don’t want minification
-    sourcemap: false, // optional, to generate sourcemaps
-    external: [
-      "fs",
-      "path",
-      "react-swipeable",
-      "@byte-creators/utils",
-      "aria-hidden",
-      "react-remove-scroll",
-      "zlib",
-      "stream",
-      "react-google-recaptcha",
-      "react",
-      "react-dom",
-      "next",
-      "next/link",
-      "react/jsx-runtime",
-    ], // specify external dependencies if needed
-    loader: {
-      ".ts": "ts", // Load TypeScript files
-      ".tsx": "tsx", // Load TSX files
-    },
-  })
-  .catch(() => process.exit(1));
+const esbuild = require("esbuild");
 
-generateDeclarations();
+const sharedConfig = {
+    entryPoints: ["src/index.ts", "./src/assets/icons/components/index.ts"],
+    bundle: true,
+    platform: "browser",
+    target: "es2015",
+    minify: true,
+    sourcemap: false,
+    external: [
+        "fs",
+        "path",
+        "react-swipeable",
+        "@byte-creators/utils",
+        "aria-hidden",
+        "react-remove-scroll",
+        "zlib",
+        "stream",
+        "react-google-recaptcha",
+        "react",
+        "react-dom",
+        "next",
+        "next/link",
+        "react/jsx-runtime",
+    ],
+    loader: {
+        ".ts": "ts",
+        ".tsx": "tsx",
+    },
+};
+
+Promise.all([
+    // Build for ESM
+    esbuild.build({
+        ...sharedConfig,
+        outdir: "dist/esm",
+        format: "esm",
+    }),
+    // Build for CJS
+    esbuild.build({
+        ...sharedConfig,
+        outdir: "dist/cjs",
+        format: "cjs",
+    }),
+])
+    .then(() => {
+        generateDeclarations();
+        console.log("Build completed!");
+    })
+    .catch(() => process.exit(1));
