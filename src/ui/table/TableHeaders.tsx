acce8b5b7f1a1c62534e97e6f549e15470c1aaba
tapuru <pogladtapira@gmail.com>
2024-12-25 15:45:10 +0500

    fix: fix table header div in p bug

--- src/ui/table/TableHeaders.tsx
+++ src/ui/table/TableHeaders.tsx
@@ -1,24 +1,24 @@
-import {TableHeader} from './Table'
-import {cn} from "@byte-creators/utils";
+import { TableHeader } from './Table'
+import { cn } from "@byte-creators/utils";
 
 type Props = {
-    className?: string
-    headers: TableHeader[]
+  className?: string
+  headers: TableHeader[]
 }
-export const TableHeaders = ({className, headers}: Props) => {
-    const headersItemStyle = cn(
-        'pl-6 pr-16 table-cell align-middle sm:pr-0 sm:pl-3 lg:pl-6',
-        className
-    )
+export const TableHeaders = ({ className, headers }: Props) => {
+  const headersItemStyle = cn(
+    'pl-6 pr-16 table-cell align-middle sm:pr-0 sm:pl-3 lg:pl-6',
+    className
+  )
 
-    return headers.map((header, ind) => {
-        return (
-            <li className={cn(headersItemStyle)} key={ind}>
-                <p className={'flex items-center'}>
-                    <span className={'mr-[6px]'}>{header.name}</span>
-                    {!!header.sort && header.sort}
-                </p>
-            </li>
-        )
-    })
+  return headers.map((header, ind) => {
+    return (
+      <li className={cn(headersItemStyle)} key={ind}>
+        <div className={'flex items-center'}>
+          <span className={'mr-[6px]'}>{header.name}</span>
+          {!!header.sort && header.sort}
+        </div>
+      </li>
+    )
+  })
 }
