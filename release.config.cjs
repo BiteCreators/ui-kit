1f6daaeb439463f1c8d71ea3e7e6338a483c2083
DePacos <depacos@yandex.ru>
2024-12-28 23:56:08 +0300

    fix: test semantic-realise

--- release.config.cjs
+++ release.config.cjs
@@ -3,11 +3,11 @@ module.exports = {
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
-    '@semantic-release/changelog',
+    '@semantic-release/npm',
     [
       '@semantic-release/git',
       {
-        assets: ['package.json', 'CHANGELOG.md'],
+        assets: ['package.json'],
         message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
       },
     ],
