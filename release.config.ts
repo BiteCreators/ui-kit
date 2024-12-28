d8c5aa299bc1fbeeadff056be9d825c3a61953f3
DePacos <depacos@yandex.ru>
2024-12-28 23:36:09 +0300

    fix: test semantic-realise

---
+++ release.config.ts
@@ -0,0 +1,25 @@
+module.exports = {
+  branches: ['main'],
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/changelog',
+    [
+      '@semantic-release/git',
+      {
+        assets: ['package.json', 'CHANGELOG.md'],
+        message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+      },
+    ],
+    [
+      '@semantic-release/github',
+      {
+        assets: ['dist/**/*.{js,css}', 'docs/**'],
+      },
+    ],
+  ],
+};
+
+// fix: --- PATCH
+// feat: --- MINOR
+// BREAKING CHANGE: --- MAJOR
