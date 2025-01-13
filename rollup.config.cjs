3d014cba34d90d2fa897b9ca9983a2e5f4f02ce3
fireonex <mari.krulikovskaya@mail.ru>
2025-01-13 16:45:23 +0300

    fix: add ignore use client warning

--- rollup.config.cjs
+++ rollup.config.cjs
@@ -1,12 +1,9 @@
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
-import replace from '@rollup/plugin-replace';
 import image from "@rollup/plugin-image";
-// import dts from "rollup-plugin-dts";
 import terser from "@rollup/plugin-terser";
 import peerDepsExternal from "rollup-plugin-peer-deps-external";
-
 import postcss from "rollup-plugin-postcss";
 
 const packageJson = require("./package.json");
@@ -30,10 +27,6 @@ export default [
       commonjs(),
       image(),
       typescript({ tsconfig: "./tsconfig.json" }),
-      replace({
-        preventAssignment: true,
-        "use client": "",
-      }),
       terser(),
       postcss(),
     ],
@@ -50,5 +43,15 @@ export default [
       "next",
       /^next\//,
     ],
+    onwarn(warning, warn) {
+      if (
+          warning.message &&
+          warning.message.includes('use client') ||
+          warning.code === "MIXED_EXPORTS"
+      ) {
+        return;
+      }
+      warn(warning);
+    }
   },
 ];
