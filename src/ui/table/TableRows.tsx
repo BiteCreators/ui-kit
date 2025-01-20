1cf724096ab317addcc1ad7c4827d2e778e48919
fireonex <mari.krulikovskaya@mail.ru>
2025-01-20 16:26:45 +0300

    IN-327 feat: add skeletons in Table

--- src/ui/table/TableRows.tsx
+++ src/ui/table/TableRows.tsx
@@ -9,8 +9,8 @@ type Props = {
 
 export const TableRows = ({ className, tableData }: Props) => {
   const tableCellStyle = cn(
-    "flex justify-between sm:table-cell sm:pr-3 sm:pl-3 sm:border-b sm:border-dark-500 sm:align-middle",
-    className,
+      "flex justify-between sm:table-cell sm:pr-3 sm:pl-3 sm:border-b sm:border-dark-500 sm:align-middle",
+      className
   );
 
   return tableData.map((element, ind: number) => {
@@ -18,21 +18,21 @@ export const TableRows = ({ className, tableData }: Props) => {
 
     for (const key in element) {
       rowCells.push(
-        <li className={tableCellStyle} key={key}>
-          <span className={"font-bold sm:font-normal"}>{element[key]}</span>
-        </li>,
+          <li className={tableCellStyle} key={key}>
+            <span className={"font-bold sm:font-normal"}>{element[key]}</span>
+          </li>
       );
     }
 
     return (
-      <ul
-        className={
-          "min-h-[200px] font-normal flex flex-col py-5 px-[10px] gap-y-3 bg-dark-500 border border-dark-300 rounded-sm sm:table-row sm:h-12 sm:bg-inherit"
-        }
-        key={ind}
-      >
-        {rowCells}
-      </ul>
+        <ul
+            className={
+              "min-h-[200px] font-normal flex flex-col py-5 px-[10px] gap-y-3 bg-dark-500 border border-dark-300 rounded-sm sm:table-row sm:h-12 sm:bg-inherit"
+            }
+            key={ind}
+        >
+          {rowCells}
+        </ul>
     );
   });
 };
