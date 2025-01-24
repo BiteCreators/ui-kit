d2a85d8eb3f52a7b59364e294d973acfb5888734
fireonex <mari.krulikovskaya@mail.ru>
2025-01-24 16:28:25 +0300

    fix: fix default avatar img

--- src/ui/avatar/Avatar.tsx
+++ src/ui/avatar/Avatar.tsx
@@ -1,77 +1,78 @@
-import React, { useState } from "react";
-import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
+import React, { useState } from 'react'
+import Skeleton, { SkeletonTheme } from 'react-loading-skeleton'
 
-import { cn } from "@byte-creators/utils";
-import Link from "next/link";
+import { cn } from '@byte-creators/utils'
+import Link from 'next/link'
 
-import { Close } from "../../assets/icons/components";
+import { Close } from '../../assets/icons/components'
+import example from './examples/exampleAvatar.png'
 
 type Props = {
-  alt?: string;
-  avatarURL: string;
-  className?: string;
-  href?: string;
-  imgStyles?: string;
-  isNextLink?: boolean;
-  onClose?: () => void;
-  rel?: string;
-  rounded?: boolean;
-  showClose?: boolean;
-};
+  alt?: string
+  avatarURL: string
+  className?: string
+  href?: string
+  imgStyles?: string
+  isNextLink?: boolean
+  onClose?: () => void
+  rel?: string
+  rounded?: boolean
+  showClose?: boolean
+}
 
 export const Avatar = ({
-  alt = "Avatar",
+  alt = 'Avatar',
   avatarURL,
   className,
   href,
   imgStyles,
   isNextLink = false,
   onClose,
-  rel = "",
+  rel = '',
   rounded = true,
   showClose = false,
 }: Props) => {
-  const [isLoaded, setIsLoaded] = useState(false);
+  const [isLoaded, setIsLoaded] = useState(false)
 
   const closeHandler = () => {
     if (onClose) {
-      onClose();
+      onClose()
     }
-  };
+  }
 
   const AvatarImage = (
-    <SkeletonTheme baseColor={"#3f3e3e"} highlightColor={"#575656"}>
+    <SkeletonTheme baseColor={'#3f3e3e'} highlightColor={'#575656'}>
       <span
-        className={cn(["relative inline-block", className])}
-        style={{ height: "100%", width: "100%" }}
+        className={cn(['relative inline-block', className])}
+        style={{ height: '100%', width: '100%' }}
       >
         {!isLoaded && (
           <Skeleton
             circle={rounded}
-            containerClassName={"absolute top-0 left-0 w-full h-full"}
-            height={"100%"}
-            width={"100%"}
+            containerClassName={'absolute top-0 left-0 w-full h-full'}
+            height={'100%'}
+            width={'100%'}
           />
         )}
         <img
           alt={alt}
-          className={cn(imgStyles, [rounded && "rounded-full"])}
+          className={cn(imgStyles, [rounded && 'rounded-full'])}
           onError={() => setIsLoaded(true)}
           onLoad={() => setIsLoaded(true)}
-          src={avatarURL || ""}
-          style={{ visibility: isLoaded ? "visible" : "hidden" }}
+          src={avatarURL || example}
+          style={{ visibility: isLoaded ? 'visible' : 'hidden' }}
         />
         {showClose && (
           <span
             className={cn([
-              "bg-dark-700 flex justify-center items-center absolute w-9 h-9 right-0 top-[4%]",
-              rounded && "rounded-full",
+              'bg-dark-700 flex justify-center items-center absolute w-9 h-9 right-0 top-[4%]',
+              rounded && 'rounded-full',
             ])}
             onClick={closeHandler}
           >
             <span
               className={
-                "flex items-center justify-center bg-danger-500 rounded-full hover:cursor-pointer"
+                'flex items-center justify-center bg-danger-500 rounded-full hover:cursor-pointer'
               }
             >
               <Close />
@@ -80,15 +81,15 @@ export const Avatar = ({
         )}
       </span>
     </SkeletonTheme>
-  );
+  )
 
   return isNextLink && href ? (
-    <Link className={"max-w-full h-full text-center"} href={href} rel={rel}>
+    <Link className={'max-w-full h-full text-center'} href={href} rel={rel}>
       {AvatarImage}
     </Link>
   ) : (
-    <a className={"max-w-full h-full text-center flex"} href={href} rel={rel}>
+    <a className={'max-w-full h-full text-center flex'} href={href} rel={rel}>
       {AvatarImage}
     </a>
-  );
-};
+  )
+}
--- src/ui/loader/LinearLoader.tsx
+++ src/ui/loader/LinearLoader.tsx
@@ -1,17 +1,17 @@
-import React from "react";
+import React from 'react'
 
 type Props = {
-  isLoading: boolean;
-};
+  isLoading: boolean
+}
 
 export const LinearLoader = ({ isLoading }: Props) => {
   if (!isLoading) {
-    return null;
+    return null
   }
 
   return (
-    <div className={`linear-loader ${isLoading ? "active" : ""}`}>
-      <div className={"linear-loader-bar"} />
+    <div className={`linear-loader ${isLoading ? 'active' : ''}`}>
+      <div className={'linear-loader-bar'} />
     </div>
-  );
-};
+  )
+}
