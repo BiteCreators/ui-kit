9211651743463486ee650c64182536152ab52b7f
DmitryChues <dmitrychues2312@gmail.com>
2025-01-26 11:46:44 +0300

    add skeleton for user profile

--- src/ui/user-profile/UserProfile.tsx
+++ src/ui/user-profile/UserProfile.tsx
@@ -1,3 +1,5 @@
+import Skeleton, { SkeletonTheme } from 'react-loading-skeleton'
+
 import { cn } from '@byte-creators/utils'
 import Link from 'next/link'
 
@@ -7,22 +9,37 @@ import { Typography } from '../typography/Typography'
 type Props = {
   avatarUrl: string
   className?: string
+  isLoading?: boolean
   profileId: number
   userName: string
 }
 
-export const UserProfile = ({ avatarUrl, className, profileId, userName }: Props) => {
+export const UserProfile = ({
+  avatarUrl,
+  className,
+  isLoading = false,
+  profileId,
+  userName,
+}: Props) => {
   return (
-    <div className={cn(className, 'flex gap-3 items-center')}>
-      <div className={'w-9 h-9'}>
-        <Avatar avatarURL={avatarUrl || ''} href={`/profile/${profileId}`} isNextLink />
+    <SkeletonTheme baseColor={'#3f3e3e'} highlightColor={'#575656'}>
+      <div className={cn(className, 'flex gap-3 items-center')}>
+        <div className={'w-9 h-9'}>
+          <Avatar avatarURL={avatarUrl || ''} href={`/profile/${profileId}`} isNextLink />
+        </div>
+        <div>
+          {isLoading ? (
+            <Skeleton height={20} width={150} />
+          ) : (
+            <Link
+              className={'hover:text-primary-300 text-light-100 duration-75'}
+              href={`/profile/${profileId}`}
+            >
+              <Typography variant={'h3'}>{userName}</Typography>
+            </Link>
+          )}
+        </div>
       </div>
-      <Link
-        className={'hover:text-primary-300 text-light-100 duration-75'}
-        href={`/profile/${profileId}`}
-      >
-        <Typography variant={'h3'}>{userName}</Typography>
-      </Link>
-    </div>
+    </SkeletonTheme>
   )
 }
