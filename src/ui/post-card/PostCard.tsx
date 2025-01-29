774eafbb7ca22c54388e288f9cf0c3590b9dc90e
fireonex <mari.krulikovskaya@mail.ru>
2025-01-29 09:09:57 +0300

    IN-324 init: init post card animation task

--- src/ui/post-card/PostCard.tsx
+++ src/ui/post-card/PostCard.tsx
@@ -1,6 +1,6 @@
 import { useGetRelativeTime, useShowMore } from '@byte-creators/utils'
 import Link from 'next/link'
-
+import { motion } from 'framer-motion'
 import { Block, Image } from '../../assets/icons/components'
 import { Button } from '../button/Button'
 import { Typography } from '../typography/Typography'
@@ -41,7 +41,7 @@ export const PostCard = ({
   })
 
   return (
-    <div className={'hover:scale-105 duration-75 max-w-[400px]'}>
+    <div className={'max-w-[400px]'} style={{ position: 'relative' }}>
       <div className={'cursor-pointer'}>
         {!postImageUrl ? (
           <Link href={`/profile/${ownerId}/publications/${postId}`}>
@@ -51,39 +51,75 @@ export const PostCard = ({
           </Link>
         ) : (
           <Link href={`/profile/${ownerId}/publications/${postId}`}>
-            <img
-              alt={postImageUrl}
-              height={postImageHight}
-              src={postImageUrl}
-              width={postImageWidth}
-            />
+            <motion.div
+              style={{
+                height: postImageHight,
+                maxHeight: '400px',
+                overflow: 'hidden',
+                position: 'relative',
+              }}
+            >
+              <motion.img
+                alt={postImageUrl}
+                animate={{ y: isCollapsed ? 0 : -50 }} // Двигаем изображение
+                height={postImageHight}
+                initial={{ y: 0 }}
+                src={postImageUrl}
+                style={{
+                  clipPath: isCollapsed ? 'none' : 'inset(0 0 50% 0)', // Обрезаем изображение
+                  objectFit: 'cover',
+                }}
+                transition={{ stiffness: 100, type: 'spring' }}
+                width={postImageWidth}
+              />
+            </motion.div>
           </Link>
         )}
       </div>
-      <div className={'mt-3 flex gap-3 items-center justify-between'}>
-        <UserProfile avatarUrl={avatarOwner} profileId={ownerId} userName={userName} />
-        {isAdmin && (
-          <Button className={'pr-0 bg-transparent'} onClick={onClickBlockButton} variant={'icon'}>
-            <Block />
-          </Button>
-        )}
-      </div>
-      <Typography className={'mt-3 text-light-900'} variant={'small-text'}>
-        {relativeTime}
-      </Typography>
-      <Typography className={'mt-[3px] break-all'}>
-        {textToShow}
-        {collapsable && (
-          <span
-            className={
-              'text-primary-500 underline underline-offset-2 cursor-pointer hover:text-primary-300'
-            }
-            onClick={toggleShowMore}
-          >
-            {isCollapsed ? 'Show more' : 'Hide'}
-          </span>
-        )}
-      </Typography>
+
+      <motion.div
+        animate={{
+          y: isCollapsed ? 0 : -30, // Поднимаем весь блок, когда раскрываем текст
+        }}
+        initial={{ y: 0 }} // Изначально все элементы находятся на одном уровне
+        style={{ overflow: 'hidden' }}
+        transition={{ duration: 0.3 }}
+      >
+        <div className={'mt-3 flex gap-3 items-center justify-between'}>
+          <UserProfile avatarUrl={avatarOwner} profileId={ownerId} userName={userName} />
+          {isAdmin && (
+            <Button className={'pr-0 bg-transparent'} onClick={onClickBlockButton} variant={'icon'}>
+              <Block />
+            </Button>
+          )}
+        </div>
+        <Typography className={'mt-3 text-light-900'} variant={'small-text'}>
+          {relativeTime}
+        </Typography>
+
+        <motion.div
+          animate={{
+            y: isCollapsed ? 0 : -30,
+          }}
+          initial={{ y: 0 }}
+          style={{ overflow: 'hidden' }}
+          transition={{ duration: 0.3 }}
+        >
+          <Typography className={'mt-[3px] break-all'}>
+            {textToShow}
+            {collapsable && (
+              <span
+                className={
+                  'text-primary-500 underline underline-offset-2 cursor-pointer hover:text-primary-300'
+                }
+                onClick={toggleShowMore}
+              >
+                {isCollapsed ? 'Show more' : 'Hide'}
+              </span>
+            )}
+          </Typography>
+        </motion.div>
+      </motion.div>
     </div>
   )
 }
